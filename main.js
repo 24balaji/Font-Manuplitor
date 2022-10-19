6ab9bc09def24cf203a872b9ef34bd160711e2ce
24balaji <99334076+24balaji@users.noreply.github.com>
2022-10-19 18:11:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+function preload()
+{
+
+}
+
+function setup()
+{
+canvas = createCanvas(390 , 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(350,300);
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+
+function draw() 
+{
+    background('#969A97');
+    textSize(difference);
+    fill(13996);
+    text('BALAJI',noseX,noseY);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialised!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = "+ leftWristX + " rightWristX = " + rightWristX + "difference");
+    }
+}
+
